4c2d4fadf0b06a98477b370d6fdeb777b3cbfd41
shahtanu126 <shahtanu126@gmail.com>
2025-06-04 19:08:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,60 +1,129 @@
-import cv2
+# Python In-built packages
+from pathlib import Path
+import PIL
+import os
+from collections import Counter
+
+# External packages
 import streamlit as st
-from ultralytics import YOLO
 
-def app():
-    st.header('Object Detection Web App')
-    st.subheader('Powered by YOLOv8')
-    st.write('Welcome!')
-    model = YOLO('yolov8n.pt')
-    object_names = list(model.names.values())
+# Local Modules
+import settings
+import helper
 
-    with st.form("my_form"):
-        uploaded_file = st.file_uploader("Upload video", type=['mp4'])
-        selected_objects = st.multiselect('Choose objects to detect', object_names, default=['person']) 
-        min_confidence = st.slider('Confidence score', 0.0, 1.0)
-        st.form_submit_button(label='Submit')
-            
-    if uploaded_file is not None: 
-        input_path = uploaded_file.name
-        file_binary = uploaded_file.read()
-        with open(input_path, "wb") as temp_file:
-            temp_file.write(file_binary)
-        video_stream = cv2.VideoCapture('video.mp4')
-        width = int(video_stream.get(cv2.CAP_PROP_FRAME_WIDTH)) 
-        height = int(video_stream.get(cv2.CAP_PROP_FRAME_HEIGHT)) 
-        fourcc = cv2.VideoWriter_fourcc(*'h264') 
-        fps = int(video_stream.get(cv2.CAP_PROP_FPS)) 
-        output_path = input_path.split('.')[0] + '_output.mp4' 
-        out_video = cv2.VideoWriter(output_path, int(fourcc), fps, (width, height)) 
+# Setting page layout
+st.set_page_config(
+    page_title="Object Detection using YOLOv8",
+    page_icon="🤖",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
 
-        with st.spinner('Processing video...'): 
-            while True:
-                ret, frame = video_stream.read()
-                if not ret:
-                    break
-                result = model(frame)
-                for detection in result[0].boxes.data:
-                    x0, y0 = (int(detection[0]), int(detection[1]))
-                    x1, y1 = (int(detection[2]), int(detection[3]))
-                    score = round(float(detection[4]), 2)
-                    cls = int(detection[5])
-                    object_name =  model.names[cls]
-                    label = f'{object_name} {score}'
+# Main page heading
+st.title("Object Detection using YOLOv8")
 
-                    if model.names[cls] in selected_objects and score > min_confidence:
-                        cv2.rectangle(frame, (x0, y0), (x1, y1), (255, 0, 0), 2)
-                        cv2.putText(frame, label, (x0, y0 - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-                
-                detections = result[0].verbose()
-                cv2.putText(frame, detections, (10, 10),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  
-                out_video.write(frame) 
-            video_stream.release()
-            out_video.release()
-        st.video(output_path)
+# Sidebar
+st.sidebar.header("ML Model Config")
 
-if __name__ == "__main__":
-    app()
+# Model Options
+model_type = st.sidebar.radio(
+    "Select Task", ['Detection'])
+
+confidence = float(st.sidebar.slider(
+    "Select Model Confidence", 25, 100, 40)) / 100
+
+# Selecting Detection Or Segmentation
+if model_type == 'Detection':
+    model_path = Path(settings.DETECTION_MODEL)
+
+# Load Pre-trained ML Model
+try:
+    model = helper.load_model(model_path)
+except Exception as ex:
+    st.error(f"Unable to load model. Check the specified path: {model_path}")
+    st.error(ex)
+
+st.sidebar.header("Image/Video Config")
+source_radio = st.sidebar.radio(
+    "Select Source", settings.SOURCES_LIST)
+
+source_imgs = []
+
+class_names_file = 'weights/class_names.txt'  # Replace with the actual path
+with open(class_names_file, 'r') as f:
+    class_names = f.read().splitlines()
+
+# Create a dictionary mapping class IDs to class names
+CLASS_NAMES_DICT = {i: name for i, name in enumerate(class_names)}
+
+# If image is selected
+if source_radio == settings.IMAGE:
+    uploaded_images = st.sidebar.file_uploader(
+        "Upload multiple images...", type=("jpg", "jpeg", "png", 'bmp', 'webp'), accept_multiple_files=True)
+
+    if uploaded_images:
+        source_imgs.extend(uploaded_images)
+
+# CLASS_NAMES_DICT=
+
+if source_imgs:
+    detect_button = st.sidebar.button("Detect Objects for All Images")
+
+    if detect_button:
+        class_counts_all_images = {}  # Dictionary to store class counts for all images
+
+        for i, source_img in enumerate(source_imgs, start=1):
+            col1, col2 = st.columns(2)
+
+            with col1:
+                try:
+                    uploaded_image = PIL.Image.open(source_img)
+                    st.image(uploaded_image, caption="Uploaded Image",
+                             use_column_width=True)
+                except Exception as ex:
+                    st.error("Error occurred while opening the image.")
+                    st.error(ex)
+
+            with col2:
+                if source_img is None:
+                    # Create a default detected image with a message
+                    default_detected_image = PIL.Image.new(
+                        'RGB', (500, 500), (255, 255, 255))
+                    draw = PIL.ImageDraw.Draw(default_detected_image)
+                    draw.text((50, 50), "No image uploaded and detected.",
+                              fill=(0, 0, 0))
+                    st.image(default_detected_image, caption='Detected Image',
+                             use_column_width=True)
+                else:
+                    res = model.predict(uploaded_image, conf=confidence)
+                    boxes = res[0].boxes
+                    # Extract the names of detected objects
+                    names = res[0].names
+                    res_plotted = res[0].plot()[:, :, ::-1]
+                    st.image(res_plotted, caption='Detected Image',
+                             use_column_width=True)
+
+                    try:
+                        with st.expander(f"Image {i} Detection Results"):
+                            object_count = len(boxes)
+                            st.write(
+                                f"Number of objects detected: {object_count}")
+
+                            # Count and display object names
+                            object_name_counts = {}
+                            for name in names:
+                                object_name = CLASS_NAMES_DICT.get(name, name)
+                                object_name_counts[object_name] = object_name_counts.get(
+                                    object_name, 0) + 1
+
+                            st.write("Object Counts:")
+                            for name, count in object_name_counts.items():
+                                # Use st.text() to display results
+                                st.text(f"{name}: {count}")
+
+                            # Store class counts for this image in the dictionary
+                            class_counts_all_images[f"Image {i}"] = object_name_counts.copy(
+                            )
+
+                    except Exception as ex:
+                        st.write("No objects detected!")
